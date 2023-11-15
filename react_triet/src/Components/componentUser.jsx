9f33bb7de1e0a46b209f74eb91666a19cce2ecab
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2023-11-15 21:35:14 +0700

    update

--- react_triet/src/Components/componentUser.jsx
+++ react_triet/src/Components/componentUser.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 
 const UserComponent = () => {
-  // Sử dụng useState để tạo state cho mảng
+
   const [numberArray, setNumberArray] = useState([]);
 
-  // Hàm thêm số ngẫu nhiên vào mảng
   const addRandomNumber = () => {
-    const randomNumber = Math.floor(Math.random() * 100); // Số ngẫu nhiên từ 0 đến 99
+    const randomNumber = Math.floor(Math.random() * 100);
 
-    // Sử dụng setNumberArray để cập nhật mảng
     setNumberArray((prevNumberArray) => [...prevNumberArray, randomNumber]);
   };
 
