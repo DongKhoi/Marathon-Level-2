740abcf35eba79ad78066d8b69fec46f4cdb861a
Trieu <nhtrieu2k5@gmail.com>
2023-12-21 20:49:53 +0700

    Buổi-35

--- demo-project/src/components/home.js
+++ demo-project/src/components/home.js
@@ -1,20 +1,44 @@
+// Home.js
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import React, { useState, useEffect, useCallback } from 'react';
+// import apiService from '../services/apiService'; // Đường dẫn tùy thuộc vào cấu trúc thư mục của bạn
 
 const Home = () => {
-  const [mainContent, setMainContent] = useState("");
-  const handleHomeClick = useCallback(() => {
-    setMainContent("This is the home page content.");
-  }, []);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // const fetchPosts = async () => {
+  //   const postsData = await apiService.fetchPosts();
+  //   setPosts(postsData);
+  //   setLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, []); // Empty dependency array means this effect runs once after initial render
+
   return (
     <div>
       <h2>Home</h2>
-      <p>{mainContent}</p>
+      {/* Render the posts */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+            // Thay đổi "id" và "title" thành các trường tương ứng trong dữ liệu API của bạn
+          ))}
+        </ul>
+      )}
+
       {/* Use Link to navigate to the "/about" route */}
       <Link to="/about">
         <button>Go to About</button>
       </Link>
-      <button onClick={handleHomeClick}>Set content</button>
+      <Link to="/redux">
+        <button>Go to Redux</button>
+      </Link>
     </div>
   );
 };
---
+++ demo-project/src/components/reduxComponent.js
@@ -0,0 +1,40 @@
+// ReduxComponent.js
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removeTodo } from '../buoi-35/actions';
+import { Link } from 'react-router-dom';
+
+const ReduxComponent = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+
+  const handleAddTodo = () => {
+    dispatch(addTodo('New Todo'));
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatch(removeTodo(id));
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}> {/* Use todo.id as the key */}
+            {todo.text}
+            <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <Link to="/">
+        <button>Go to Home</button>
+      </Link>
+      <Link to="/redux-thunk">
+        <button>Go to Redux Thunk</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ReduxComponent;
